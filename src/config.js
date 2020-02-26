3abe7a7443fffd74ecf77ec65f0329b830838ad2
Anthony Hill <antdavhill@gmail.com>
2020-02-26 15:14:43 -0600

    api has been deployed and work on endpoints is starting

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: 'something',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://whibut-client.now.sh/',
 }
