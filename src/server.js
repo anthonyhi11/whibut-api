b619c95809b69f4d1b1afa55a3a5e18b3961c3a5
Anthony Hill <antdavhill@gmail.com>
2020-02-26 13:50:02 -0600

    initial deploy

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
-const app = require('./app');
-const { PORT } = require('./config');
+require('dotenv').config();
+const knex = require('knex');
+const app = require('./app')
+const { PORT, DB_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
