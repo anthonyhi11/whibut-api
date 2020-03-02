c74ed5137a5a4ae2a3c0aa5f8227ce01ca6efc2f
Anthony Hill <antdavhill@gmail.com>
2020-03-02 11:18:30 -0600

    added some explicitreturns to login, api works with postman but not chrome local host

--- src/login/login-router.js
+++ src/login/login-router.js
@@ -10,34 +10,35 @@ loginRouter
     const { username, password } = req.body;
     const loginUser = { username, password};
   for (const [key, value] of Object.entries(loginUser))
-    if (value == null)
+    if (value == null) {
       return res.status(400).json({
         error: `Missing '${key}' in request body`
       })
+    }
   LoginService.getUserWithUsername(
     req.app.get('db'),
     loginUser.username
   )
   .then(dbUser => {
     if (!dbUser) {
-      return res.status(400).json({
+     return res.status(400).json({
         error: `Incorrect username or password`
       })
-    }
+    } else {
     return LoginService.comparePasswords(loginUser.password, dbUser.password)
       .then(compareMatch => {
         if (!compareMatch) {
-          return res.status(400).json({
+         return res.status(400).json({
             error: `Incorrect username or password`
           })
         }
-        
         const sub = dbUser.username
         const payload = { user_id: dbUser.id }
         res.send({
           authToken: LoginService.createJwt(sub, payload),
         })
       })
+    }
     })
     .catch(next)
   })
